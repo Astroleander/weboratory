1d7f719446ea34f63ea65570af3259bc908eb726
Astroleander <astroleander@gmail.com>
2020-04-08 16:46:29 +0800

    add data files to this scenario

---
+++ src/lab-scenario/views/chess-model/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  pieces: [
+    {
+      name: 'Queen',
+      file: 'Queen.glb',
+      describe: 'The ♛ is the most powerful piece in the game of chess.'
+    },
+    {
+      name: 'Bishop',
+      file: 'Bishop.glb',
+      describe: 'Each player begins the game with two ♝. One starts between the king\'s knight and the king, the other between the queen\'s knight and the queen.'
+    },
+    {
+      name: 'Pawn',
+      file: 'Pawn.glb',
+      describe: ''
+    },
+
+    {
+      name: 'Rook',
+      file: 'Rook.glb',
+      describe: ''
+    },
+    {
+      name: 'King',
+      file: 'King.glb',
+      describe: ''
+    },
+    {
+      name: 'Knight',
+      file: 'knight.glb',
+      describe: ''
+    },
+  ]
+}
