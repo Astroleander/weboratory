942144e5a5d42fda581a3e7c740c5e603e24fb58
Astroleander <astroleander@gmail.com>
2020-04-15 02:07:21 +0800

    add comment

--- src/snippet/algorithm/leetcode/math/1025.divisor-game.js
+++ src/snippet/algorithm/leetcode/math/1025.divisor-game.js
@@ -14,12 +14,28 @@
  */
 
 /**
- * @recursion who take the last one step
- * 1. get all divisors
- * 2. strategy(N) = strategy(d1) || strategy(d2) || ... || strategy(1)
+ * @key
+ * @see https://leetcode.com/problems/divisor-game/discuss/514275/JS-simple-explanation
+ * odd = odd * odd
+ * even = odd * even
+ * even = even * even
+ * so if odd have divisors, they must be odds,
  * 
- * @bound
- * 0 < x < N and N % x == 0
+ * odd - odd = even;
+ * so then N is odd, we always give opponent an even number.
+ * 
+ * but for even we at least have divisor 1, 2
+ * even - odd = odd;
+ * even - even = even;
+ * we can give opponent always an odd number
+ * 
+ * so if we start with an odd number, we'd always give opponent an even number
+ * and none of number's divisor will bigger then its 1/2,
+ * so we always approching N(3)
+ * 
+ * 一句话中文: 处理奇数的人永远给对面偶数，偶数可以永远给对面奇数
+ * 最后都会收敛回 N(3) 和 N(2),
+ * 奇数永远输
  */
 
 /**
