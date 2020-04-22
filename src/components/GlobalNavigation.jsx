aa5c4ada91b1eaeed7177a7fbd6cc43cdddb5f42
Astroleander <astroleander@gmail.com>
2020-04-23 00:22:21 +0800

    add react global nav

---
+++ src/components/GlobalNavigation.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function generateList(r) {
+  return r.keys().map((link) => {
+    let name = link.split("/")[1];
+    console.log(name)
+    if (name === "home") return;
+    return (
+      <a href={`${name}/`} 
+        className="react-global-nav-link"
+        key={name}
+        >
+        {name.replace(/^\w*?-/, "").toUpperCase()}
+      </a>
+    );
+  });
+};
+
+const GlobalNavigation = () => {
+  console.log("[GlobalNavigation] load...");
+  return (
+    <nav>
+      {generateList(require.context("@/", true, /index.html$/, "lazy"))}
+    </nav>
+  );
+}
+
+export default GlobalNavigation;
