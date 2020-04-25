0dd61654ed1bb0c0b6cc7fd62e04d79e36bee02a
Astroleander <astroleander@gmail.com>
2020-04-26 03:02:22 +0800

    fixed reg

--- src/lab-framework/router/index.js
+++ src/lab-framework/router/index.js
@@ -13,7 +13,7 @@ function importAll(prefix, r) {
 
 importAll(
   '',
-  require.context("../views/", true, /index.jsx$/, "lazy")
+  require.context("../views/", true, /index\.jsx$/, "lazy")
 );
 
 export default page_lists;
