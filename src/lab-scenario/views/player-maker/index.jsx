af7b236fd1d38899b56ed05e2e6fe9334857a97a
Astroleander <astroleander@gmail.com>
2020-05-19 21:30:07 +0800

    make dice works

--- src/lab-scenario/views/player-maker/index.jsx
+++ src/lab-scenario/views/player-maker/index.jsx
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
 
-import PlayerGenerator, { defaultSeed } from './PlayerGenerator'
+import PlayerGenerator, { getNewSeed } from './PlayerGenerator'
 import PlayerDigital from "./PlayerDigital";
 import PlayerList from "./PlayerList";
 
 import './style.scss'
 
+const defaultSeed = (()=>getNewSeed())();
 export class PlayerMaker extends Component {
   constructor(props) {
     super(props);
-    console.log(defaultSeed)
     this.state = {
-      seed: defaultSeed
+      seed: defaultSeed,
+      playerlist: [],
     }
+    this.index = 0
+    this.player = null
+  }
+  updateSeed () {
+    this.setState({seed: (() => getNewSeed())()})    
+  }
+  shouldComponentUpdate(nextProps, nextStates) {
+    console.log(nextStates, this.state)
+    return true;
   }
   render() {
     return (
       <article className='container'>
+        {console.log(this.state.seed)}
         <PlayerGenerator onClick={e => setTimeout(() => this.setState({seed: e}), 0)}></PlayerGenerator>
-        <PlayerDigital seed={this.state.seed}></PlayerDigital>
-        <PlayerList></PlayerList>
+        <PlayerDigital seed={this.state.seed} setPlayer={(p)=>{this.player = p}}></PlayerDigital>
+        <PlayerList 
+          list={this.state.playerlist} 
+          onClick={e=> {
+            acceptPlayer(this, this.player);
+            this.updateSeed();
+          }}
+          deleteItem={idx=> {
+            this.state.playerlist.splice(idx, 1)
+            this.setState({playerlist: this.state.playerlist})
+          }
+          }></PlayerList>
       </article>
     );
   }
 }
 
-
+const acceptPlayer = (ctx, e) => {
+  ctx.state.playerlist.push(e);
+  ctx.setState({playerlist: ctx.state.playerlist})
+}
 
 
 function setID(id) {
