fd7c80fbd5acca958d5e28c2e666afe983234310
Astroleander <astroleander@gmail.com>
2020-06-10 10:49:04 +0800

    formated

--- src/lab-framework/views/lifecycle/index.jsx
+++ src/lab-framework/views/lifecycle/index.jsx
@@ -14,17 +14,17 @@ console.log(
 console.groupEnd("[LifeCYCLE]");
 console.time("[LAST]");
 
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useRef } from "react";
 /** es6 not support static arrow props given */
 class LifecycleMonitor {}
 LifecycleMonitor.log = (...args) => {
-    console.group("[LifeCYCLE]");
-    console.timeEnd("[LAST]");
-    console.timeLog("[TOTAL]");
-    console.log("%c[LifeCYCLE]", "color:red", ...args);
-    console.groupEnd("[LifeCYCLE]");
-    console.time("[LAST]");
-}
+  console.group("[LifeCYCLE]");
+  console.timeEnd("[LAST]");
+  console.timeLog("[TOTAL]");
+  console.log("%c[LifeCYCLE]", "color:red", ...args);
+  console.groupEnd("[LifeCYCLE]");
+  console.time("[LAST]");
+};
 
 /** You need forcibly refresh page at this route to trigger listeners below */
 document.addEventListener("DOMContentLoaded", () => {
@@ -36,21 +36,21 @@ document.addEventListener("readystatechange", () =>
 
 window.onload = () => {
   LifecycleMonitor.log("[window] window.onload FIRED");
-}
+};
 window.onunload = () => {
   LifecycleMonitor.log("[window] window.onunload FIRED");
 };
 window.onbeforeunload = () => {
   LifecycleMonitor.log("[window] window.onbeforeunload FIRED");
   // return false; /** prevent default navigation event */
-  return true
-}
+  return true;
+};
 
 const iframeFactroy = ({
-  id = 'iframe',
-  width = '60%',
-  height = '60%',
-  src = '../../index.html',
+  id = "iframe",
+  width = "60%",
+  height = "60%",
+  src = "../../index.html",
 } = {}) => {
   LifecycleMonitor.log("[iframe] creating...");
   const iframe = document.createElement("iframe");
@@ -60,35 +60,40 @@ const iframeFactroy = ({
   iframe.src = src;
   iframe.onload = () => {
     LifecycleMonitor.log("[iframe] onload");
-  }
+  };
   setTimeout(() => {
     // iframe.src = src;
-    LifecycleMonitor.log("[iframe][setTimeout]", 'won\'t block');
-  }, 5000)
+    LifecycleMonitor.log("[iframe][setTimeout]", "won't block");
+  }, 5000);
   LifecycleMonitor.log("[iframe] created");
-  return iframe
-}
+  return iframe;
+};
 LifecycleMonitor.log("[react] compile react fucntional component");
+
 const Lifecycle = () => {
   let iframe = iframeFactroy();
   let container = useRef(null);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     LifecycleMonitor.log("[react] useEffect called");
-    container.current.appendChild(iframe)
-  })
+    container.current.appendChild(iframe);
+  });
   return (
     <>
-      <section className="focus-center" ref={container} onLoad={()=>LifecycleMonitor.log('[section] loaded')}>
+      <section 
+        className="focus-center" ref={container}
+        onLoad={() => LifecycleMonitor.log("[section] loaded")}
+      >
         <div>
           <code>F12</code> to open console to see monitor output
-          <button onClick={() => {window.onunload = null;}}> cancel unload preventing</button>
+          <button onClick={() => { window.onunload = null; }}>
+            cancel unload preventing
+          </button>
         </div>
       </section>
     </>
   );
-}
+};
 
 export default Lifecycle;
 LifecycleMonitor.log("[file] end");
