462d60e69ddc4775b4ddaa4b3233ed9a66ed1f4d
Astroleander <astroleander@gmail.com>
2020-06-10 11:01:58 +0800

    used hooks

---
+++ src/lab-framework/views/useHook/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UseReducerSample from "./useReducerLogin";
+
+export default function index() {
+  return (
+    <div>
+      <UseReducerSample></UseReducerSample>
+    </div>
+  );
+}
