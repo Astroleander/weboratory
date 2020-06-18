e81198b256d01aec6c8f4a051d7e9b6b51a545a0
Astroleander <astroleander@gmail.com>
2020-06-18 20:52:15 +0800

    deleted unexisted dependencies

--- src/lab-scenario/views/chess-model/index.jsx
+++ src/lab-scenario/views/chess-model/index.jsx
@@ -4,8 +4,6 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-import { Tools } from '@/utils/function.js'
-
 import './index.scss'
 import CONFIG from './config'
 
