f5e31b51f2192c4406fb8dc8133daa62b3992dba
Astroleander <astroleander@gmail.com>
2020-07-02 14:42:12 +0800

    init learn-redux

--- src/lab-react/views/redux/index.jsx
+++ src/lab-react/views/redux/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import PageA from './PageA';
+import PageB from './PageB';
 export default function ReduxTest() {
   return (
     <div>
       <h1>Redux Test</h1>
+      <hr />
+      <PageA></PageA>
+      <hr />
+      <PageB></PageB>
     </div>
   )
 }
