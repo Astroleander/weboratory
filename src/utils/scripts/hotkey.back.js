2977ac3e743c4c260ec13903f55411ccaf588bd2
Astroleander <astroleander@gmail.com>
2020-08-27 14:27:00 +0800

    [feat] compatible mac back key

--- src/utils/scripts/hotkey.back.js
+++ src/utils/scripts/hotkey.back.js
@@ -1,5 +1,5 @@
 document.onkeydown = function(e) {
-  if ( e.keyCode === 90 && e.ctrlKey ) {
+  if ( e.keyCode === 90 && ( e.ctrlKey || e.metaKey) ) {
     console.log('[global] back key down')
     history.back();
   }
