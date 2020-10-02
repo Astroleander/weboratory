0f87c0a0f5e2eb8beb9a6175619f7bce0199732b
Astroleander <astroleander@gmail.com>
2020-10-03 00:13:03 +0800

    [fomat] code auto format

--- src/lab-algorithm/layouts/AlgoLayout.jsx
+++ src/lab-algorithm/layouts/AlgoLayout.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import FABG from '@/components/FABG';
+import React from "react";
+import FABG from "@/components/FABG";
 export default class AlgoLayout extends React.Component {
   constructor(props) {
     super(props);
     let pathname = this.props.location.pathname;
-    let path = pathname.substring(pathname.indexOf('/', 2));
+    let path = pathname.substring(pathname.indexOf("/", 2));
     /** 👇 Webapck 使用实时 import 的时候经常需要限定范围 */
-    import('@/snippet/algorithm' + path).then(m => {
+    import("@/snippet/algorithm" + path).then((m) => {
       let state = { path };
       let module = m.default || m;
       console.log(module);
 
-      Object.keys(module).forEach(propertyName => {
+      Object.keys(module).forEach((propertyName) => {
         /** copy each param */
-        state[propertyName] = m.default[propertyName]
+        state[propertyName] = module[propertyName];
         /** parse name and params of function */
-        if (propertyName.startsWith('__')) {
+        if (propertyName.startsWith("__")) {
           // no action
-        } else if (propertyName !== 'inputs') { /** 👈 means the property is the function (so our functions name can't be 'inputs') */
-          state['__func_key'] = propertyName
-        } else { /** 👈 means the property is inputs */
-          state['__inputs_type'] = [...module[propertyName].map(propertyName => typeof propertyName)]
+        } else if (propertyName !== "inputs") {
+          /** 👈 means the property is the function (so our functions name can't be 'inputs') */
+          state["__func_key"] = propertyName;
+        } else {
+          /** 👈 means the property is inputs */
+          state["__inputs_type"] = [
+            ...module[propertyName].map((propertyName) => typeof propertyName),
+          ];
         }
-      })
-      state['__done'] = true;
+      });
+      state["__done"] = true;
       this.setState(state);
-    })
+    });
   }
   /** 在参数加载完成之前不进行渲染，即不执行 render 中的 f 函数，避免莫名其妙的错误 */
   shouldComponentUpdate(nextProps, nextState) {
-    return nextState['__done']
+    return nextState["__done"];
   }
   render() {
     /** [📖 review] precedence of Logic OR(||) is 5, and Logic AND(&&) is 6 */
@@ -37,59 +41,70 @@ export default class AlgoLayout extends React.Component {
     if (!func_key) return null;
     return (
       <>
-      {(
-        <>
-          <p>{this.renderDescriptions()}</p>
-          <pre>{String(this.state[func_key])}</pre>
-          <hr />
-          {/** delay timing of params render until function has loaded*/}
-          {this.state[func_key] && this.state['inputs'] && this.state['__inputs_type'] && this.renderArgs()}
-          <hr />
-          {this.state[func_key] && this.state['inputs'] && this.renderResults(func_key)}
-        </>
-      )}
-      <FABG />
+        {
+          <>
+            <p>{this.renderDescriptions()}</p>
+            <pre>{String(this.state[func_key])}</pre>
+            <hr />
+            {/** delay timing of params render until function has loaded*/}
+            {this.state[func_key] &&
+              this.state["inputs"] &&
+              this.state["__inputs_type"] &&
+              this.renderArgs()}
+            <hr />
+            {this.state[func_key] &&
+              this.state["inputs"] &&
+              this.renderResults(func_key)}
+          </>
+        }
+        <FABG />
       </>
-    )
+    );
   }
   renderArgs() {
     /** display all params and create input for primitive type params */
-    return this.state['inputs'].map((item, idx) => {
+    return this.state["inputs"].map((item, idx) => {
       return (
-        <div key={`${idx}`} className='float-label-container'>
+        <div key={`${idx}`} className="float-label-container">
           <input
             id={`args-${idx}`}
-            value={this.state['inputs'][idx]}
+            value={this.state["inputs"][idx]}
             onChange={(e) => this.handleArgChange(e.target.value, idx, e)}
-            disabled={(this.state['__inputs_type'][idx] === 'number' || this.state['__inputs_type'][idx] === 'string' ? '' : 'disabled')}
+            disabled={
+              this.state["__inputs_type"][idx] === "number" ||
+              this.state["__inputs_type"][idx] === "string"
+                ? ""
+                : "disabled"
+            }
           />
           <label htmlFor={`arg-${idx}`}>
             <code>
-            {typeof this.state['inputs'][idx] === 'object' ?
-              JSON.stringify(this.state['inputs'][idx]) : this.state['inputs'][idx]}
-          </code>
+              {typeof this.state["inputs"][idx] === "object"
+                ? JSON.stringify(this.state["inputs"][idx])
+                : this.state["inputs"][idx]}
+            </code>
           </label>
         </div>
-      )
+      );
     });
   }
   renderResults(key) {
     let f = this.state[key];
     return (
-      <div>result:
-        <blockquote>
-          {JSON.stringify(f(...this.state['inputs']))}
-        </blockquote>
+      <div>
+        result:
+        <blockquote>{JSON.stringify(f(...this.state["inputs"]))}</blockquote>
       </div>
-    )
+    );
   }
   renderDescriptions() {
+    console.log(this.state);
     return this.state.__description ?? null;
   }
   handleArgChange(val, idx, e) {
-    let newer = this.state['inputs'];
-    if (this.state['__inputs_type'][idx] === 'number') {
-      /** 
+    let newer = this.state["inputs"];
+    if (this.state["__inputs_type"][idx] === "number") {
+      /**
        * 1. if string endswith '-' means user enter '-' just now
        * 2. currently, val is like 'xxx-', newer[idx] is the old value xxx (number)
        * 3. so we could just set val to negtive
