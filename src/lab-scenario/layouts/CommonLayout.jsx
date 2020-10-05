5bef172e2f581382f33958a9bbd5b777259b619f
Astroleander <astroleander@gmail.com>
2020-10-05 19:02:58 +0800

    [update] fit vue 2.6 constuctor

--- src/lab-scenario/layouts/CommonLayout.jsx
+++ src/lab-scenario/layouts/CommonLayout.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import FABG from '@/components/FABG';
 
 export class CommonLayout extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = { component: null };
   }
   componentDidMount(nextProps, prevState, snapshot) {
